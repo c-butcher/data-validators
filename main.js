7ff91692fb2a6ee0277ac6cd9c4b0b5c2646ac5e
Chris Butcher <c.butcher@hotmail.com>
2018-11-27 20:31:01 -0800

    Updated it so the Validator loads all the validators when called.

--- main.js
+++ main.js
@@ -1,24 +1,31 @@
 const Validator = require('./src/validator');
-const ValidationError = require('./src/validation-error');
-
-const ZipCode = require('./src/validators/zip-code');
-const Length = require('./src/validators/length');
-const Range = require('./src/validators/range');
+const Boolean = require('./src/validators/boolean');
 const Choice = require('./src/validators/choice');
 const Contains = require('./src/validators/contains');
+const Length = require('./src/validators/length');
+const Number = require('./src/validators/number');
+const Range = require('./src/validators/range');
+const String = require('./src/validators/string');
+const ZipCode = require('./src/validators/zip-code');
 
-Validator.add('zip-code', ZipCode);
-Validator.add('length', Length);
-Validator.add('range', Range);
-Validator.add('choice', Choice);
-Validator.add('contains', Contains);
+Validator
+    .add('boolean', Boolean)
+    .add('choice', Choice)
+    .add('contains', Contains)
+    .add('length', Length)
+    .add('number', Number)
+    .add('range', Range)
+    .add('string', String)
+    .add('zip-code', ZipCode);
 
 module.exports = {
     Validator,
-    ValidationError,
-    ZipCode,
-    Length,
-    Range,
+    Boolean,
     Choice,
     Contains,
+    Length,
+    Number,
+    Range,
+    String,
+    ZipCode,
 };
