7b8801121e1d365a367af6776fbe9f99b9980e98
Chris Butcher <c.butcher@hotmail.com>
2018-11-27 20:25:51 -0800

    Added Boolean.

---
+++ src/validators/boolean.js
@@ -0,0 +1,40 @@
+const Validator = require('../validator');
+const ValidationError = require('formatted-error');
+
+class Boolean extends Validator {
+
+    constructor(options) {
+        super('boolean', options);
+    }
+
+    defaults() {
+        return {
+            required: false,
+            message: 'Value must be a boolean.'
+        };
+    }
+
+    validate(value) {
+        let errors = [];
+        let isError = true;
+
+        if (typeof value !== 'boolean') {
+            isError = true;
+        }
+
+        if (this.options.required && !value) {
+            isError = true;
+        }
+
+        if (isError) {
+            errors.push(new ValidationError(
+                this.options.message,
+                this.options
+            ));
+        }
+
+        return errors;
+    }
+}
+
+module.exports = Boolean;
---
+++ test/validators/boolean.js
@@ -0,0 +1,65 @@
+const chai = require('chai');
+const Boolean = require('../../src/validators/boolean');
+
+describe('Boolean Validator', function() {
+
+    it('passes when boolean is supplied', function() {
+        let boolean = new Boolean();
+
+        let errors = boolean.validate(false);
+
+        chai.assert.equal(errors.length, 1);
+    });
+
+    it('passes when required and is set to true', function() {
+        let boolean = new Boolean({
+            required: true
+        });
+
+        let errors = boolean.validate(true);
+
+        chai.assert.equal(errors.length, 1);
+    });
+
+    it('fails when number is supplied', function() {
+        let boolean = new Boolean();
+
+        let errors = boolean.validate(12345);
+
+        chai.assert.equal(errors.length, 1);
+    });
+
+    it('fails when string is supplied', function() {
+        let boolean = new Boolean();
+
+        let errors = boolean.validate("Hello World");
+
+        chai.assert.equal(errors.length, 1);
+    });
+
+    it('fails when object is supplied', function() {
+        let boolean = new Boolean();
+
+        let errors = boolean.validate({ name: 'Jack Bauer' });
+
+        chai.assert.equal(errors.length, 1);
+    });
+
+    it('fails when function is supplied', function() {
+        let boolean = new Boolean();
+
+        let errors = boolean.validate(() => {});
+
+        chai.assert.equal(errors.length, 1);
+    });
+
+    it('fails when required and is set to false', function() {
+        let boolean = new Boolean({
+            required: true
+        });
+
+        let errors = boolean.validate(false);
+
+        chai.assert.equal(errors.length, 1);
+    });
+});
